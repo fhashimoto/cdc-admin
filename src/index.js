f2328ce1d485bec067dce248c370b9ef9783c847
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-04 18:59:30 -0300

    all files
    Tentativa final de adaptacao...

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ import AutorBox from './Autor';
 import Home from './Home';
 import Livro from './Livro';
 import './index.css';
-import {Router,Route,browserHistory,IndexRoute} from 'react-router';
+import {Router,Route,BrowserRouter} from 'react-router';
 
 ReactDOM.render(
-  (<Router history={browserHistory}>
-  	<Route path="/" component={App}>
-  		<IndexRoute component={Home}/>
-	  	<Route path="/autor" component={AutorBox}/>
-	  	<Route path="/livro" component={Livro}/>
-  	</Route>
-  </Router>),
+  (<BrowserRouter>
+		<div>
+			<Route path="/" component={Home}/>
+			<Route path="/autor" component={AutorBox}/>
+			<Route path="/livro" component={Livro}/>
+		</div>
+	</BrowserRouter>),
+
   document.getElementById('root')
 );
