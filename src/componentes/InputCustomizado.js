56f56a856f2ee96d5d9f95da42956acd14b5a19d
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-04 14:19:41 -0300

    Create InputCustomizado.js
    Criando o inputcustomizado

---
+++ src/componentes/InputCustomizado.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class InputCustomizado extends Component{
+    render(){
+        return(  
+        <div className='pure-control-group'>
+            <label htmlFor='nome'>Nome</label>
+            <input id='nome' type='text' name='nome' value={this.state.nome} onChange={this.setNome}></input>
+        </div>
+        );
+    }
+}
+
