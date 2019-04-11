1d65d7b8e4386ef15177d597edf5fdc72389c823
Felipe Hashimoto <felipe.hashi@gmail.com>
2019-04-10 22:14:00 -0300

    Update App.js
    Teste

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           
           <a href="#menu" id="menuLink" className="menu-link">
               
-              <span></span>
+              <span>Teste</span>
           </a>
 
           <div id="menu">
@@ -21,9 +21,7 @@ class App extends Component {
                   <ul className="pure-menu-list">
                       <li className="pure-menu-item"><Link to="/" className="pure-menu-link">Home</Link></li>
                       <li className="pure-menu-item"><Link to="/autor" className="pure-menu-link">Autor</Link></li>
-                      <li className="pure-menu-item"><Link to="/livro" className="pure-menu-link">Livro</Link></li>
-
-                      
+                      <li className="pure-menu-item"><Link to="/livro" className="pure-menu-link">Livro</Link></li>                     
                   </ul>
               </div>
           </div>
